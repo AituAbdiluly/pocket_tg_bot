ae1c749a118c64fb28593585cb0f3ceec61986ac
AituAbdiluly <aituufs@gmail.com>
2021-02-26 16:36:07 +0100

    Add link handler

--- pkg/telegram/handlers.go
+++ pkg/telegram/handlers.go
@@ -1,9 +1,11 @@
 package telegram
 
 import (
-	"log"
+	"context"
+	"net/url"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/zhashkevych/go-pocket-sdk"
 )
 
 const (
@@ -12,13 +14,34 @@ const (
 	replyAlreadyAuthorized = "You have already been authorized. Now you can use Pocket Bot. Send some links :)"
 )
 
-func (b *Bot) handleMessage(message *tgbotapi.Message) {
-	log.Printf("[%s] %s", message.From.UserName, message.Text)
+func (b *Bot) handleMessage(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Link is saved to your Pocket.")
 
-	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
-	msg.ReplyToMessageID = message.MessageID
+	_, err := url.ParseRequestURI(message.Text)
+	if err != nil {
+		msg.Text = "This is not valid link!"
+		_, err = b.bot.Send(msg)
+		return err
+	}
 
-	b.bot.Send(msg)
+	accessToken, err := b.getAccessToken(message.Chat.ID)
+	if err != nil {
+		msg.Text = "You are not authorized! Use command /start to authorize."
+		_, err = b.bot.Send(msg)
+		return err
+	}
+
+	if err := b.pocketClient.Add(context.Background(), pocket.AddInput{
+		AccessToken: accessToken,
+		URL:         message.Text,
+	}); err != nil {
+		msg.Text = "Oops, failed to add a the link. Try again later."
+		_, err = b.bot.Send(msg)
+		return err
+	}
+
+	_, err = b.bot.Send(msg)
+	return err
 }
 
 func (b *Bot) handleCommand(message *tgbotapi.Message) error {
